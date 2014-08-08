168da36478f207ad091a937d260ddbab200cbb96
Keerthana KT <keerthanakt@gmail.com>
2014-08-08 14:52:42 +0530

    Update TrackingNumberGame.java

--- TrackingNumberGame.java
+++ TrackingNumberGame.java
@@ -1,6 +1,8 @@
 package com.TrackingNumber;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.io.*;
 
 class TrackList {
@@ -33,6 +35,18 @@ class TrackList {
 	{
 		this.range=range;
 	}
+	public int getStartingNumber()
+	{
+		return range.getStartingNumber();
+	}
+	public char getStatus()
+	{
+		return this.status;
+	}
+	public int getTransferCode()
+	{
+		return this.transferCode;
+	}
 }
 
 public class TrackingNumberGame {
@@ -60,40 +74,78 @@ public class TrackingNumberGame {
 			inputTrackRecords.add(record);
 		}
 		tracks.add(inputTrackRecords.get(0));
-		
+      		
 		reader.close();
 
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		parseInput("input.txt");	
+		parseInput("C:\\Users\\kekt\\Downloads\\teatcase.txt");	
+		
+		processInput();
 		
 	}
 	
 
 	public static void processInput(){
-		Range rangeObject=new Range();
+		
 		for (int i = 1; i < inputTrackRecords.size(); i++){
-			TrackList input = inputTrackRecords.get(i);
-			for(TrackList track: tracks){
-				if(track.range.contains(input.range)){
-					//split
-				}
-				else if(input.range.contains(track.range)){
-					//split();
-				}
-				//else if(){ // condition need to be returned
-					// overlapping
-			//	}
-				else if(input.isStatusEquals(track) && input.isTrackCodeEquals(track) && rangeObject.isContinuous(input.range,track.range))
-				{
-				   track.setRange(rangeObject.mergeTwoRanges(input.range, track.range));
-				}
+          processTracks(inputTrackRecords.get(i));
+          sort();
+	}
+	System.out.println(tracks);	
+	}
+	public static void processTracks(TrackList item)
+	{
+		TrackList input = item;
+		Range rangeObject=new Range();
+		for(TrackList track: tracks){
+			if(rangeObject.isOverlapping(track.range,input.range))
+			{
+				ArrayList<Range> r = rangeObject.splitOverlappingRanges(track.range,input.range);
+				processTracks(new TrackList(r.get(2),input.getStatus(),input.getTransferCode()));
+				tracks.add(new TrackList(r.get(1),track.getStatus(),track.getTransferCode()));
+				track.setRange(r.get(0));
 				
 			}
-		}
+			else if(input.range.contains(track.range)){
+				    ArrayList<Range> r = rangeObject.splitConstituentRanges(track.range,input.range);
+					track.setRange(r.get(0));
+					tracks.add(input);
+					tracks.add(new TrackList(r.get(2),track.getStatus(),track.getTransferCode()));						
+					
+			}
+			else
+				tracks.add(input);
+			
+			
+		
 	}
+	}
+		private static void sort()
+		{
+			Collections.sort(tracks, new Comparator<TrackList>(){
+                public int compare(TrackList  r1, TrackList r2){
+                   return (r1.getStartingNumber() - r2.getStartingNumber());
+              }});
+		}
+		private void merge()
+		{
+			Range rangeObject = new Range();
+		 for(int i = 0; i < tracks.size()-1; i++)
+		 {
+			 TrackList input = tracks.get(i);
+			 TrackList nextTrack = tracks.get(i+1);
+			if(input.isStatusEquals(nextTrack) && input.isTrackCodeEquals(nextTrack) && rangeObject.isContinuous(input.range,nextTrack.range))
+			{
+				input.setRange(rangeObject.mergeTwoRanges(input.range, nextTrack.range));
+			    tracks.remove(i+1);
+			    i=i-1;
+			}
+	
+		 }
+		}
 	
 	
 
