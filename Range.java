06d991c8d109739e257836e3d0e1b4e0482f2e44
vishal456 <vishal2177346@gmail.com>
2014-08-08 10:38:36 +0530

    Range Class Commit

---
+++ Range.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Range {
+	int startingNumber;
+	int endingNumber;
+	
+	private Range(){
+		
+	}
+	
+	public Range(int start, int end){
+		startingNumber	= start;
+		endingNumber = end;
+	}
+	
+	public boolean isOverlapping(Range firstRange, Range secondRange){
+		if(firstRange.startingNumber < secondRange.startingNumber && firstRange.endingNumber > secondRange.startingNumber)
+			return true;
+		if(secondRange.startingNumber < firstRange.startingNumber && secondRange.endingNumber > firstRange.startingNumber)
+			return true;
+		return false;
+	}
+	
+	public boolean isContinous(Range firstRange, Range secondRange){
+		if(firstRange.endingNumber + 1 == secondRange.startingNumber)
+			return true;
+		if(secondRange.endingNumber + 1 == firstRange.startingNumber)
+			return true;
+		return false;
+	}
+	
+	public boolean contains(Range newRange){
+		if(this.startingNumber < newRange.startingNumber && this.endingNumber > newRange.endingNumber)
+			return true;
+		return false;
+	}
+	
+	public Range mergeTwoRanges(Range firstRange, Range secondRange){
+		if(firstRange.endingNumber + 1 == secondRange.startingNumber){
+			firstRange.endingNumber = secondRange.endingNumber;
+			return firstRange;
+		}
+		if(secondRange.endingNumber + 1 == firstRange.startingNumber){
+			secondRange.endingNumber = firstRange.endingNumber;
+			return secondRange;
+		}
+		return firstRange;
+	}
+	
+	public ArrayList< Range > splitConstituentRanges(Range dominantRange, Range recessiveRange){
+		ArrayList<Range> list = new ArrayList<Range>();
+		if(dominantRange.contains(recessiveRange)){
+			list.add(dominantRange);
+			return list;
+		}
+		else{
+			list.add(new Range(recessiveRange.startingNumber, dominantRange.startingNumber - 1));
+			list.add(dominantRange);
+			list.add(new Range(dominantRange.endingNumber + 1, recessiveRange.endingNumber));
+			return list;
+		}
+	}
+	
+	public ArrayList< Range > splitOverlappingRanges(Range dominantRange, Range recessiveRange){
+		ArrayList<Range> list = new ArrayList<Range>();
+		list.add(dominantRange);
+		if(dominantRange.startingNumber < recessiveRange.startingNumber){
+			list.add(new Range(dominantRange.endingNumber + 1, recessiveRange.endingNumber));
+		}
+		else{
+			list.add(new Range(recessiveRange.startingNumber, dominantRange.startingNumber - 1));
+		}
+		return list;
+		
+	}
+}
